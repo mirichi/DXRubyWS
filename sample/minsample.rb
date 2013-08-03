afeb7ea52cc52422a6081901aed6d8f74a3f772e
mirichi <sawara01@gmail.com>
2013-08-03 21:24:39 +0900

    サンプルをコミットするの忘れてた。

--- sample/minsample.rb
+++ sample/minsample.rb
@@ -6,8 +6,23 @@ b = WS::WSButton.new(10,30,100,20)
 l = WS::WSLabel.new(10,70,100,20)
 w.add_control(b)
 w.add_control(l)
+
+image1 = Image.new(30,30,C_WHITE)
+image2 = Image.new(30,30,C_BLACK)
+i = WS::WSImage.new(200,30, image1)
+i.add_handler(:mouse_over) do |obj|
+  obj.image = image2
+end
+i.add_handler(:mouse_out) do |obj|
+  obj.image = image1
+end
+
+w.add_control(i)
+
 WS::desktop.add_control(w)
 
 Window.loop do
   WS.update
+  break if Input.key_push?(K_ESCAPE)
 end
+
