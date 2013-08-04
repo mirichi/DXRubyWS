9185b9f1ea0765680aac1ae5e2612c2dd10f074c
mirichi <sawara01@gmail.com>
2013-08-04 14:14:09 +0900

    :clickシグナルにも反応するようにしてみた

--- sample/minsample.rb
+++ sample/minsample.rb
@@ -9,12 +9,16 @@ w.client.add_control(l)
 
 image1 = Image.new(30,30,C_WHITE)
 image2 = Image.new(30,30,C_BLACK)
+image3 = Image.new(30,30,C_RED)
 i = WS::WSImage.new(200,30, image1)
 i.add_handler(:mouse_over) do |obj|
   obj.image = image2
 end
 i.add_handler(:mouse_out) do |obj|
   obj.image = image1
+end
+i.add_handler(:click) do |obj|
+  obj.image = image3
 end
 w.client.add_control(i)
 
