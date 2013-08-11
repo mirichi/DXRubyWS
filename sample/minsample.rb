7997eab61da1bba5963ca7c60c82bb8a2b31ef27
mirichi <sawara01@gmail.com>
2013-08-11 12:16:26 +0900

    サンプルちょこっと修正

--- sample/minsample.rb
+++ sample/minsample.rb
@@ -1,31 +1,25 @@
 require 'dxruby'
 require_relative '../lib/dxrubyws'
 
-w = WS::WSWindow.new(100,100,300,100)
-b = WS::WSButton.new(10,10,100,20)
-l = WS::WSLabel.new(10,50,100,20)
+w = WS::WSWindow.new(100, 100, 300, 100)
+b = WS::WSButton.new(10, 10, 100, 20)
+l = WS::WSLabel.new(10, 50, 100, 20)
 w.client.add_control(b)
 w.client.add_control(l)
 
-image1 = Image.new(30,30,C_WHITE)
-image2 = Image.new(30,30,C_BLACK)
-image3 = Image.new(30,30,C_RED)
+image1 = Image.new(30, 30, C_WHITE)
+image2 = Image.new(30, 30, C_BLACK)
+image3 = Image.new(30, 30, C_RED)
 i = WS::WSImage.new(200, 30, 30, 30)
 i.image = image1
-i.add_handler(:mouse_over) do |obj|
-  obj.image = image2
-end
-i.add_handler(:mouse_out) do |obj|
-  obj.image = image1
-end
-i.add_handler(:click) do |obj|
-  obj.image = image3
-end
+i.add_handler(:mouse_over){|obj|obj.image = image2}
+i.add_handler(:mouse_out){|obj|obj.image = image1}
+i.add_handler(:click){|obj|obj.image = image3}
 w.client.add_control(i)
 
-WS::desktop.add_control(w)
+WS.desktop.add_control(w)
 
-w = WS::WSWindow.new(400,300,300,100)
+w = WS::WSWindow.new(400, 300, 300, 100)
 WS::desktop.add_control(w)
 
 Window.loop do
