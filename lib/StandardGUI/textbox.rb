e6914affdf91f044de224e3669ae733c32d3f416
mirichi <sawara01@gmail.com>
2013-09-08 22:50:01 +0900

    テキストボックスにShift+カーソルキーでの範囲選択機能を追加

--- lib/StandardGUI/textbox.rb
+++ lib/StandardGUI/textbox.rb
@@ -18,56 +18,128 @@ module WS
                 .line(@width-1,0,@width-1,@height-1,[240,240,240])
                 .line(0,@height-1,@width-1,@height-1,[240,240,240])
       @text = ""
-      @cursor_count = 0
-      @cursor_pos = 0
-      @active = false
+      @cursor_count = 0 # カーソル点滅用カウント
+      @cursor_pos = 0   # カーソル位置
+      @active = false   # フォーカスがあるかどうか
+      @selected_range_first = nil # 範囲選択始点
+      @selected_range_last = nil  # 範囲選択終点
       @font = Font.new(12)
 
       # 特殊キーのハンドラ
       add_key_handler(K_BACKSPACE) do
-        if @cursor_pos > 0
-          @text[@cursor_pos-1] = ""
-          @cursor_pos -= 1
+        if @selected_range_first
+          min = [@selected_range_first, @selected_range_last].min
+          max = [@selected_range_first, @selected_range_last].max-1
+          @text[min..max] = ""
+          @selected_range_first = nil
+          @cursor_pos = min
+        else
+          if @cursor_pos > 0
+            @text[@cursor_pos-1] = ""
+            @cursor_pos -= 1
+          end
         end
       end
 
       add_key_handler(K_DELETE) do
-        if @cursor_pos <= @text.length
+        if @selected_range_first
+          min = [@selected_range_first, @selected_range_last].min
+          max = [@selected_range_first, @selected_range_last].max-1
+          @text[min..max] = ""
+          @selected_range_first = nil
+          @cursor_pos = min
+        else
           @text[@cursor_pos] = ""
         end
       end
 
       add_key_handler(K_LEFT) do
         if @cursor_pos > 0
+          if Input.key_down?(K_LSHIFT) or Input.key_down?(K_RSHIFT)
+            if @selected_range_first
+              @selected_range_last = @cursor_pos-1
+            else
+              @selected_range_first = @cursor_pos
+              @selected_range_last = @cursor_pos-1
+            end
+            @selected_range_first = nil if @selected_range_first == @selected_range_last
+          else
+            @selected_range_first = nil
+          end
           @cursor_pos -= 1
         end
       end
 
       add_key_handler(K_RIGHT) do
         if @cursor_pos < @text.length
+          if Input.key_down?(K_LSHIFT) or Input.key_down?(K_RSHIFT)
+            if @selected_range_first
+              @selected_range_last = @cursor_pos+1
+            else
+              @selected_range_first = @cursor_pos
+              @selected_range_last = @cursor_pos+1
+            end
+            @selected_range_first = nil if @selected_range_first == @selected_range_last
+          else
+            @selected_range_first = nil
+            @selected_range_last = nil
+          end
           @cursor_pos += 1
         end
       end
 
       add_key_handler(K_HOME) do
+        if Input.key_down?(K_LSHIFT) or Input.key_down?(K_RSHIFT)
+          if @selected_range_first
+            @selected_range_last = 0
+          else
+            @selected_range_first = @cursor_pos
+            @selected_range_last = 0
+          end
+          @selected_range_first = nil if @selected_range_first == @selected_range_last
+        else
+          @selected_range_first = nil
+        end
         @cursor_pos = 0
       end
 
       add_key_handler(K_END) do
+        if Input.key_down?(K_LSHIFT) or Input.key_down?(K_RSHIFT)
+          if @selected_range_first
+            @selected_range_last = @text.length
+          else
+            @selected_range_first = @cursor_pos
+            @selected_range_last = @text.length
+          end
+          @selected_range_first = nil if @selected_range_first == @selected_range_last
+        else
+          @selected_range_first = nil
+        end
         @cursor_pos = @text.length
       end
     end
 
+    # キーが押されたらカーソル点滅カウントを初期化
     def on_key_push(key)
       @cursor_count = 0
       super
     end
 
+    # 文字が入力されたらカーソル位置に挿入
     def on_string(str)
-      @text[@cursor_pos, 0] = str
+      if @selected_range_first
+        min = [@selected_range_first, @selected_range_last].min
+        max = [@selected_range_first, @selected_range_last].max-1
+        @text[min..max] = str
+        @cursor_pos = min + str.length - 1
+      else
+        @text[@cursor_pos, 0] = str
+      end
+      @selected_range_first = nil
       @cursor_pos += str.length
     end
 
+    # カーソル点滅と位置設定処理
     def update
       if @active
         @cursor_count += 1
@@ -76,12 +148,14 @@ module WS
       end
     end
 
+    # フォーカス取得
     def on_enter
       @active = true
       Input::IME.enable = true
       Input::IME.set_font(@font)
     end
 
+    # フォーカス喪失
     def on_leave
       @active = false
       Input::IME.enable = false
@@ -89,6 +163,15 @@ module WS
 
     def draw
       super
+
+      # 選択範囲表示
+      if @selected_range_first
+        tx1 = self.x + @font.get_width(@text[0, [@selected_range_first, @selected_range_last].min]) + 4
+        tx2 = self.x + @font.get_width(@text[0, [@selected_range_first, @selected_range_last].max]) + 4
+        (0..(@font.size)).each do |ty|
+          self.target.draw_line(tx1, self.y + ty + 2, tx2, self.y + 2 + ty, [200, 200, 255])
+        end
+      end
 
       # 文字列表示
       self.target.draw_font(self.x + 4, self.y + 4, @text, @font, :color=>C_BLACK)
@@ -98,6 +181,7 @@ module WS
         tx = self.x + @font.get_width(@text[0, @cursor_pos]) + 4
         self.target.draw_line(tx, self.y + 3, tx, self.y + 2 + @font.size, C_BLACK)
       end
+
     end
   end
 end
