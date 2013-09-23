48a584faf069ec5e1452d73fc7f0c0c6e80ce95c
mirichi <sawara01@gmail.com>
2013-09-23 13:48:54 +0900

    WeakRefを使ってGC時にフォントキャッシュを破棄する

--- lib/fontcache.rb
+++ lib/fontcache.rb
@@ -1,12 +1,15 @@
 # coding: utf-8
+require 'weakref'
 
 # フォントキャッシュ
 class RenderTarget
+  @@font_cache = WeakRef.new({})
 
   def draw_font(x, y, str, font, hash)
-    @@font_cache ||= {}
+    GC.disable
     color = hash[:color]
     color = C_WHITE unless color
+    @@font_cache = WeakRef.new({}) unless @@font_cache.weakref_alive?
     if @@font_cache.has_key?(font)
       data1 = @@font_cache[font]
     else
@@ -24,5 +27,6 @@ class RenderTarget
       data2[str] = Image.new(width, font.size).draw_font(0, 0, str, font, color)
     end
     self.draw(x, y, data2[str])
+    GC.enable
   end
 end
