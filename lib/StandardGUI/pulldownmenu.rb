b70d0d2ae83867f4ad11420a771ec46bc2cf7cb1
RPGP1 <great.super.science@gmail.com>
2013-10-05 08:58:08 +0900

    Update pulldownmenu.rb

--- lib/StandardGUI/pulldownmenu.rb
+++ lib/StandardGUI/pulldownmenu.rb
@@ -1,3 +1,27 @@
 # coding: utf-8
 
 require_relative './common'
+
+module WS
+  class WSPullDownMenu < WSControl
+    def initialize(tx, ty, width, height)
+      super
+      @content = []
+      @image = Image.new(@width, @height, C_WHITE).draw_border(true)
+    end
+    
+    def resize(width, height
+      super
+      @image.dispose if @image
+      @image = @image.new(@width, @height, C_WHITE).draw_border(true)
+    end
+    
+    def on_mouse_push(tx, ty)
+      super
+    end
+    
+    def draw
+      super
+    end
+  end
+end
