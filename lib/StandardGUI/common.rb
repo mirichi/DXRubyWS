9c4a59e0c1f52a6eba79506860833dc406441be4
mirichi <sawara01@gmail.com>
2013-11-23 08:53:00 +0900

    文字列切り出しサポート追加

--- lib/StandardGUI/common.rb
+++ lib/StandardGUI/common.rb
@@ -36,22 +36,40 @@ class Image
   def draw_border(flag)
     if flag
       self.line(0,0,width-1,0,WS::C_DARK_WHITE)
-          .line(0,0,0,height-1,WS::C_DARK_WHITE)
-          .line(1,1,width-1,1,WS::C_LIGHT_GRAY)
-          .line(1,1,1,height-1,WS::C_LIGHT_GRAY)
-          .line(width-1,0,width-1,height-1,WS::C_LIGHT_BLACK)
-          .line(0,height-1,width-1,height-1,WS::C_LIGHT_BLACK)
-          .line(width-2,1,width-2,height-2,WS::C_DARK_GRAY)
-          .line(1,height-2,width-2,height-2,WS::C_DARK_GRAY)
+      self.line(0,0,0,height-1,WS::C_DARK_WHITE)
+      self.line(1,1,width-1,1,WS::C_LIGHT_GRAY)
+      self.line(1,1,1,height-1,WS::C_LIGHT_GRAY)
+      self.line(width-1,0,width-1,height-1,WS::C_LIGHT_BLACK)
+      self.line(0,height-1,width-1,height-1,WS::C_LIGHT_BLACK)
+      self.line(width-2,1,width-2,height-2,WS::C_DARK_GRAY)
+      self.line(1,height-2,width-2,height-2,WS::C_DARK_GRAY)
     else
       self.line(0,0,width-1,0,WS::C_LIGHT_BLACK)
-          .line(0,0,0,height-1,WS::C_LIGHT_BLACK)
-          .line(1,1,width-1,1,WS::C_DARK_GRAY)
-          .line(1,1,1,height-1,WS::C_DARK_GRAY)
-          .line(width-1,0,width-1,height-1,WS::C_LIGHT_GRAY)
-          .line(0,height-1,width-1,height-1,WS::C_LIGHT_GRAY)
-          .line(width-2,1,width-2,height-2,WS::C_DARK_WHITE)
-          .line(1,height-2,width-2,height-2,WS::C_DARK_WHITE)
+      self.line(0,0,0,height-1,WS::C_LIGHT_BLACK)
+      self.line(1,1,width-1,1,WS::C_DARK_GRAY)
+      self.line(1,1,1,height-1,WS::C_DARK_GRAY)
+      self.line(width-1,0,width-1,height-1,WS::C_LIGHT_GRAY)
+      self.line(0,height-1,width-1,height-1,WS::C_LIGHT_GRAY)
+      self.line(width-2,1,width-2,height-2,WS::C_DARK_WHITE)
+      self.line(1,height-2,width-2,height-2,WS::C_DARK_WHITE)
     end
+  end
+end
+
+#元々の文字列を、指定したfontで、max_width以下にギリギリ収まるように短くする
+#flagにfalseを指定すると、文字列の末尾の方を返す。
+class String
+  def within(font, max_width, flag = true)
+    str = ""
+    self.size.times do |i|
+      if flag
+        return str if font.getWidth(str + self[i]) > max_width
+        str += self[i]
+      else
+        return str if font.getWidth(self[self.size - i - 1] + str) > max_width
+        str = self[self.size - i - 1] + str
+      end
+    end
+    return self
   end
 end
