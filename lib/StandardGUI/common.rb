835ad3067d7fc569b99f865264ba27624d5aec26
narumitukasa <kagami@abish.sakura.ne.jp>
2014-05-04 21:18:46 +0900

    色変更用に色定数を追加
    試験的に枠描画の色指定を変更、互換性のために旧名も残してあります。

--- lib/StandardGUI/common.rb
+++ lib/StandardGUI/common.rb
@@ -1,10 +1,24 @@
 # coding: utf-8
 module WS
+  ### 色定数 ###
+  # 色名
   C_GRAY = [190, 190, 190]
   C_DARK_GRAY = [120,120,120]
   C_LIGHT_BLACK = [80,80,80]
   C_DARK_WHITE = [240,240,240]
   C_LIGHT_GRAY = [220,220,220]
+  # パーツ名
+  C_BASE = [190, 190, 190]               # ウィンドウやボタン等の基本色
+  C_SHADOW = [120,120,120]               # 影
+  C_DARKSHADOW = [80,80,80]              # 濃い影
+  C_LIGHT = [220,220,220]                # 明るい
+  C_HIGHLIGHT = [240,240,240]            # ハイライト
+  C_CONTROL_BASE = [255,255,255]         # テキストボックス、リストボックスなどの背景色
+  C_MARKER = [0,0,0]                     # チェックボックス、ラジオボタン等のマークの色
+  C_SELECT = [0,30,153]                  # リストボックスなどの選択色
+  C_FONT_COLOR = [0,0,0]                 # デフォルトの文字色
+  C_FONT_REVERSE_COLOR = [255, 255, 255] # 反転文字色
+  # 画像キャッシュ用ハッシュ
   IMG_CACHE    = {}
 end
 
@@ -29,23 +43,23 @@ end
 class Image
   def draw_border(flag)
     if flag
-      self.line(0,0,width-1,0,WS::C_DARK_WHITE)
-      self.line(0,0,0,height-1,WS::C_DARK_WHITE)
-      self.line(1,1,width-1,1,WS::C_LIGHT_GRAY)
-      self.line(1,1,1,height-1,WS::C_LIGHT_GRAY)
-      self.line(width-1,0,width-1,height-1,WS::C_LIGHT_BLACK)
-      self.line(0,height-1,width-1,height-1,WS::C_LIGHT_BLACK)
-      self.line(width-2,1,width-2,height-2,WS::C_DARK_GRAY)
-      self.line(1,height-2,width-2,height-2,WS::C_DARK_GRAY)
+      self.line(0,0,width-1,0,WS::C_HIGHLIGHT)
+      self.line(0,0,0,height-1,WS::C_HIGHLIGHT)
+      self.line(1,1,width-1,1,WS::C_LIGHT)
+      self.line(1,1,1,height-1,WS::C_LIGHT)
+      self.line(width-1,0,width-1,height-1,WS::C_DARKSHADOW)
+      self.line(0,height-1,width-1,height-1,WS::C_DARKSHADOW)
+      self.line(width-2,1,width-2,height-2,WS::C_SHADOW)
+      self.line(1,height-2,width-2,height-2,WS::C_SHADOW)
     else
-      self.line(0,0,width-1,0,WS::C_LIGHT_BLACK)
-      self.line(0,0,0,height-1,WS::C_LIGHT_BLACK)
-      self.line(1,1,width-1,1,WS::C_DARK_GRAY)
-      self.line(1,1,1,height-1,WS::C_DARK_GRAY)
-      self.line(width-1,0,width-1,height-1,WS::C_LIGHT_GRAY)
-      self.line(0,height-1,width-1,height-1,WS::C_LIGHT_GRAY)
-      self.line(width-2,1,width-2,height-2,WS::C_DARK_WHITE)
-      self.line(1,height-2,width-2,height-2,WS::C_DARK_WHITE)
+      self.line(0,0,width-1,0,WS::C_DARKSHADOW)
+      self.line(0,0,0,height-1,WS::C_DARKSHADOW)
+      self.line(1,1,width-1,1,WS::C_SHADOW)
+      self.line(1,1,1,height-1,WS::C_SHADOW)
+      self.line(width-1,0,width-1,height-1,WS::C_LIGHT)
+      self.line(0,height-1,width-1,height-1,WS::C_LIGHT)
+      self.line(width-2,1,width-2,height-2,WS::C_HIGHLIGHT)
+      self.line(1,height-2,width-2,height-2,WS::C_HIGHLIGHT)
     end
   end
 end
