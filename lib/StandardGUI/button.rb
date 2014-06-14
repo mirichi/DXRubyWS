27ff2440f98edf0e435ec02325703c4bee8d4b02
RPGP1 <great.super.science@gmail.com>
2014-06-14 17:57:18 +0900

    Update button.rb

--- lib/StandardGUI/button.rb
+++ lib/StandardGUI/button.rb
@@ -7,12 +7,21 @@ module WS
 
     def initialize(tx, ty, width, height, caption = "")
       super(tx, ty, width, height)
-      @image = {}
       @caption = caption
       @fore_color = COLOR[:font]
 
       # 画像を作成する
       set_image
+      
+      self.add_animation(:usual, 1, [0])
+      self.add_animation(:active, 1, [1])
+      self.add_animation(:mouseover, 1, [2])
+      self.add_animation(:mouseover_active, 1, [3])
+      self.add_animation(:pushed, 1, [4])
+      
+      @pushed = false
+      @mouseover = false
+      set_animation
     end
     
     # キャプションの設定
@@ -27,48 +36,92 @@ module WS
       # 画像を作成する
       set_image
     end
-     
-    # set_imageで@image[true](押された絵)と@image[false](通常の絵)を設定する。
-    # オーバーライドしてこのメソッドを再定義することでボタンの絵を変更することができる。
+    
     def set_image
-      # 画像を再作成する前にdisposeする
-      if @image.has_key?(true)
-        @image[false].dispose
-        @image[true].dispose
-      end
+      Image.dispose(self.animation_image)
       
-      # 通常時の画像を作成
-      @image[false] = Image.new(@width, @height, COLOR[:base]).draw_border(true)
-      # 押下時の画像を作成
-      @image[true] = Image.new(@width, @height, COLOR[:base]).draw_border(false)
+      self.animation_image = [img = Image.createFromArray(@width, @height, Array.new(@height){|y| Array.new(@width){|x|
+                                if x == 0 || x == @width - 1 || y == 0 || y == @height - 1
+                                  #画像の端なら
+                                  COLOR[:border]
+                                else
+                                  COLOR[:button][(y - 1).to_f / (@height - 2)]
+                                end
+                              }}.flatten),
+                              
+                              img.dup.draw(2, 2, IMAGE[:dotted_box][@width-4,@height-4,COLOR[:mark]]),
+                              
+                              img = Image.new(@width, @height, COLOR[:mouseover]).box(0,0,@width-1,@height-1,COLOR[:mouseover_border]),
+                              
+                              img.dup.draw(2, 2, IMAGE[:dotted_box][@width-4,@height-4,COLOR[:mark]]),
+                              
+                              Image.new(@width, @height, COLOR[:pushed]).box(0,0,@width-1,@height-1,COLOR[:pushed_border])
+                              ]
+      
       # キャプションの描画
       if @caption.length > 0
         width = @font.get_width(@caption)
-        @image[false].draw_font_ex(@width / 2 - width / 2 ,
-                             @height / 2 - @font.size / 2 ,
-                             @caption, @font, {:color => @fore_color, :aa => false})
-      
-        @image[true].draw_font_ex(@width / 2 - width / 2 + 1,
-                             @height / 2 - @font.size / 2 + 1,
-                             @caption, @font, {:color => @fore_color, :aa => false})
+        self.animation_image[0..3].each{|img|
+                               img.draw_font_ex(@width / 2 - width / 2 ,
+                               @height / 2 - @font.size / 2 ,
+                               @caption, @font, {:color => @fore_color, :aa => false})
+        }
+        
+        self.animation_image[4].draw_font_ex(@width / 2 - width / 2 + 1,
+                                @height / 2 - @font.size / 2 + 1,
+                                @caption, @font, {:color => @fore_color, :aa => false})
       end
+      
       refreshed
     end
-
+    
+    def set_animation
+      if @pushed
+        self.change_animation(:pushed)
+      else
+        if @active
+          if @mouseover
+            self.change_animation(:mouseover_active)
+          else
+            self.change_animation(:active)
+          end
+        else
+          if @mouseover
+            self.change_animation(:mouseover)
+          else
+            self.change_animation(:usual)
+          end
+        end
+      end
+      
+      self
+    end
+    
+    def on_mouse_over
+      super
+      @mouseover = true
+      set_animation
+    end
+    
+    def on_mouse_out
+      super
+      @mouseover = false
+      set_animation
+    end
+    
+    def on_enter
+      super
+      set_animation
+    end
+    
+    def on_leave
+      super
+      set_animation
+    end
+    
     def render
       set_image if refresh?
-      self.image = @image[@image_flag]
-    end
-
-    def draw
-      super
-      return unless self.image
-      if self.activated?
-        self.target.draw_line(self.x - 1, self.y - 1, self.x + @width, self.y - 1, C_BLACK)
-        self.target.draw_line(self.x - 1, self.y - 1, self.x - 1, self.y + @height, C_BLACK)
-        self.target.draw_line(self.x + @width, self.y - 1, self.x + @width, self.y + @height, C_BLACK)
-        self.target.draw_line(self.x - 1, self.y + @height, self.x + @width, self.y + @height, C_BLACK)
-      end
+      self.update_animation
     end
   end
 
@@ -79,13 +132,15 @@ module WS
 
     def on_key_push(key)
       if key == K_SPACE
-        @image_flag = true
+        @pushed = true
+        set_animation
       end
     end
 
     def on_key_release(key)
       if key == K_SPACE
-        @image_flag = false
+        @pushed = false
+        set_animation
         on_click(0, 0)
       end
     end
