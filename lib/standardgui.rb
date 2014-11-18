5447973edbf6a670a50e47b2254ac0fa4ae811c0
mirichi <sawara01@gmail.com>
2014-11-18 20:18:13 +0900

    standardguiは存在するファイルをすべて読み込む

--- lib/standardgui.rb
+++ lib/standardgui.rb
@@ -1,17 +1,3 @@
-require_relative './standardgui/windowbase'
-require_relative './standardgui/window'
-require_relative './standardgui/panel'
-require_relative './standardgui/button'
-require_relative './standardgui/checkbox'
-require_relative './standardgui/label'
-require_relative './standardgui/image'
-require_relative './standardgui/listbox'
-require_relative './standardgui/scrollbar'
-require_relative './standardgui/clickmenu'
-require_relative './standardgui/listview'
-require_relative './standardgui/textbox'
-require_relative './standardgui/messagebox'
-require_relative './standardgui/pulldownlist'
-require_relative './standardgui/numberinput'
-require_relative './standardgui/tab'
-require_relative './standardgui/radiobutton'
+Dir[File.dirname(__FILE__) + '/standardgui/' + "/*.rb"].sort.each do |path|
+  require path
+end
