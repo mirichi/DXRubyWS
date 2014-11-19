c405d926c2a9c919eb9138b54e416d27757602b3
RPGP1 <great.super.science@gmail.com>
2014-11-19 21:18:38 +0900

    Update menubar.rb
    直。

--- lib/StandardGUI/menubar.rb
+++ lib/StandardGUI/menubar.rb
@@ -1,484 +1,95 @@
-    def hbox # 水平に並べる
-      # サイズ未定のものをカウント
-      undef_size_count = @data.count{|o| o.resizable_width}
+# coding: utf-8
+require_relative './popupmenu'
+require_relative './common'
+require_relative './image'
 
-      # サイズ確定オブジェクトのサイズ合計
-      total = @data.inject(0){|t, o| t += (o.resizable_width ? 0 : o.width)}
-
-      # サイズが確定されていないオブジェクトの配列作成
-      undef_size_ctl = @data.select{|o| o.resizable_width}.sort_by{|o|o.min_width}.reverse
-
-      width = 0
-      rest = (self.width - @margin_left - @margin_right - total).to_f
-      count = undef_size_ctl.size
-
-      # サイズの大きいほうから残りのすべてをmin_widthにできるかどうかを確認する
-      undef_size_ctl.each do |o|
-        if rest < (count * o.min_width) # 入りきらない
-          rest -= o.min_width # このぶんは確定とする
-          count -= 1
-        else
-          width = rest / count
-          break
-        end
-      end
-
-      # 座標開始位置
-      point = self.x + @margin_left
-
-      case undef_size_count
-      when 0 # 均等
-        # 座標調整
-        adjust_x do |o|
-          tmp = (self.width - @margin_left - @margin_right - total) / (@data.size + 1) # オブジェクトの間隔を足す
-          point += (tmp > 0 ? tmp : 0)
-          @new_x = point
-          point += @new_width
-        end
-
-      else # 最大化するものを含む
-        # 座標調整
-        adjust_x do |o|
-          @new_x = point
-          if o.resizable_width # 最大化するオブジェクトを最大化
-            @new_width = (width < @new_min_width ? @new_min_width : width)
-          end
-          point += @new_width
-        end
-      end
-    end
+module WS
+  # ウィンドウにくっつけるメニューバー
+  class WSMenuBar < WSContainer
     
-    def vbox # 垂直に並べる
-      # サイズ未定のものをカウント
-      undef_size_count = @data.count{|o| o.resizable_height}
-
-      # サイズ確定オブジェクトのサイズ合計
-      total = @data.inject(0){|t, o| t += (o.resizable_height ? 0 : o.height)}
-
-      # サイズが確定されていないオブジェクトの配列作成
-      undef_size_ctl = @data.select{|o| o.resizable_height}.sort_by{|o|o.min_height}.reverse
-
-      height = 0
-      rest = (self.height - @margin_top - @margin_bottom - total).to_f
-      count = undef_size_ctl.size
-
-      # サイズの大きいほうから残りのすべてをmin_heightにできるかどうかを確認する
-      undef_size_ctl.each do |o|
-        if rest < (count * o.min_height) # 入りきらない
-          rest -= o.min_height # このぶんは確定とする
-          count -= 1
-        else
-          height = rest / count
-          break
-        end
-      end
-
-      # 座標開始位置
-      point = self.y + @margin_top
-
-      case undef_size_count
-      when 0 # 均等
-        # 座標調整
-        adjust_y do |o|
-          tmp = (self.height - @margin_top - @margin_bottom - total) / (@data.size + 1) # オブジェクトの間隔を足す
-          point += (tmp > 0 ? tmp : 0)
-          @new_y = point
-          point += @new_height
-        end
-
-      else # 最大化するものを含む
-        # 座標調整
-        adjust_y do |o|
-          @new_y = point
-          if o.resizable_height # 最大化するオブジェクトを最大化
-            @new_height = (height < @new_min_height ? @new_min_height : height)
-          end
-          point += @new_height
-        end
-      end
-    end
-    
-    def hbox_ex #水平に並べ、はみ出したら下の段に
-      rest = default_rest = self.width - @margin_left - @margin_right
-      lines = [[]]
+    class WSMenuBarItem < WSImage
+      attr_reader :item
+      attr_accessor :popup
       
-      @data.each do |ctl|
-        w = (ctl.resizable_width ? ctl.min_width : ctl.width)
+      def initialize(str, item, font=nil)
+        @str = str
+        @item = item
+        @font = font if font
+        w = @font.getWidth(@str)
         
-        rest -= w
-        if rest <= 0
-          if lines[-1].empty?
-            lines[-1] << ctl
-            lines << []
-          else
-            lines << [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[-1] << ctl
-        end
-      end
-      
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:hbox, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_height = lines.map(&:min_height).inject(&:+)
-      @min_width = lines.map(&:min_width).max
-      
-      @data = lines
-      
-      vbox
-    end
-    
-    def vbox_ex #上下に並べ、はみ出したら左の段へ
-      rest = default_rest = self.height - @margin_top - @margin_bottom
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_height ? ctl.min_height : ctl.height)
+        super(nil,nil,w+10,@font.size+4)
         
-        rest -= w
-        if rest <= 0
-          if lines[0].empty?
-            lines[0] << ctl
-            lines.unshift []
-          else
-            lines.unshift [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[0] << ctl
-        end
-      end
-      
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:vbox, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_width = lines.map(&:min_width).inject(&:+)
-      @min_height = lines.map(&:min_height).max
-      
-      @data = lines
-      
-      hbox
-    end
-    
-    def left #左に詰める
-      point = self.x + @margin_left
-      
-      adjust_x do |o|
-        @new_x = point
-        @new_width = @new_min_width if o.resizable_width
-        point += @new_width
-      end
-    end
-    
-    def right #右に詰める
-      total = @data.map{|ctl| ctl.resizable_width ? ctl.min_width : ctl.width}.inject(&:+)
-      
-      point = self.x + self.width - @margin_right - total
-      
-      adjust_x do |o|
-        @new_x = point
-        @new_width = (o.resizable_width ? o.min_width : o.width)
-        point += @new_width
-      end
-    end
-    
-    def hcenter #左右に並べ、中央に固める
-      total = @data.map{|ctl| ctl.resizable_width ? ctl.min_width : ctl.width}.inject(&:+)
-      
-      point = self.x + (self.width + @margin_left - @margin_right - total) / 2
-      
-      adjust_x do |o|
-        @new_x = point
-        @new_width = (o.resizable_width ? o.min_width : o.width)
-        point += @new_width
-      end
-    end
-    
-    def top #上に詰める
-      point = self.y + @margin_top
-      
-      adjust_y do |o|
-        @new_y = point
-        @new_height = @new_min_height if o.resizable_height
-        point += @new_height
-      end
-    end
-    
-    def bottom #下に詰める
-      total = @data.map{|ctl| ctl.resizable_height ? ctl.min_height : ctl.height}.inject(&:+)
-      
-      point = self.y + self.height - @margin_bottom - total
-      
-      adjust_y do |o|
-        @new_y = point
-        @new_height = (o.resizable_height ? o.min_height : o.height)
-        point += @new_height
-      end
-    end
-    
-    def vcenter #上下に並べ、中央に固める
-      total = @data.map{|ctl| ctl.resizable_height ? ctl.min_height : ctl.height}.inject(&:+)
-      
-      point = self.y + (self.height + @margin_top - @margin_bottom - total) / 2
-      
-      adjust_y do |o|
-        @new_y = point
-        @new_height = (o.resizable_height ? o.min_height : o.height)
-        point += @new_height
-      end
-    end
-    
-    def left_ex #左に詰め、はみ出したら下の段へ
-      rest = default_rest = self.width - @margin_left - @margin_right
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_width ? ctl.min_width : ctl.width)
+        @image = {true  => Image.new(w + 10, @font.size + 4).draw(4, 0, Image.new(w + 2, @font.size + 4, [150,150,150])).draw_font_ex(5, 2, @str, @font, :color=>COLOR[:font],:aa=>false),
+                  false => Image.new(w + 10, @font.size + 4).draw_font_ex(5, 2, @str, @font, :color=>COLOR[:font],:aa=>false)}
+        self.image = @image[false]
         
-        rest -= w
-        if rest <= 0
-          if lines[-1].empty?
-            lines[-1] << ctl
-            lines << []
-          else
-            lines << [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[-1] << ctl
-        end
+        @mouse_on = false
+        @popup = false
       end
       
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:left, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_height = lines.map(&:min_height).inject(&:+)
-      @min_width = lines.map(&:min_width).max
-      
-      @data = lines
-      
-      vbox
-    end
-    
-    def right_ex #右に詰め、はみ出したら下の段へ
-      rest = default_rest = self.width - @margin_left - @margin_right
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_width ? ctl.min_width : ctl.width)
+      def on_mouse_push(tx, ty)
+        @mouse_on = false
         
-        rest -= w
-        if rest <= 0
-          if lines[-1].empty?
-            lines[-1] << ctl
-            lines << []
-          else
-            lines << [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[-1] << ctl
-        end
-      end
-      
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:right, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_height = lines.map(&:min_height).inject(&:+)
-      @min_width = lines.map(&:min_width).max
-      
-      @data = lines
-      
-      vbox
-    end
-    
-    def hcenter_ex #左右に並べ、中央に固める。はみ出したら下の段へ
-      rest = default_rest = self.width - @margin_left - @margin_right
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_width ? ctl.min_width : ctl.width)
+        self.parent.popup(self)
         
-        rest -= w
-        if rest <= 0
-          if lines[-1].empty?
-            lines[-1] << ctl
-            lines << []
-          else
-            lines << [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[-1] << ctl
-        end
+        super
       end
       
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:center, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_height = lines.map(&:min_height).inject(&:+)
-      @min_width = lines.map(&:min_width).max
-      
-      @data = lines
-      
-      vbox
-    end
-    
-    def top_ex #上に詰め、はみ出したら左の段へ
-      rest = default_rest = self.height - @margin_top - @margin_bottom
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_height ? ctl.min_height : ctl.height)
+      def on_mouse_move(tx, ty)
+        @mouse_on = true
         
-        rest -= w
-        if rest <= 0
-          if lines[0].empty?
-            lines[0] << ctl
-            lines.unshift []
-          else
-            lines.unshift [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[0] << ctl
-        end
-      end
-      
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:top, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_width = lines.map(&:min_width).inject(&:+)
-      @min_height = lines.map(&:min_height).max
-      
-      @data = lines
-      
-      hbox
-    end
-    
-    def bottom_ex #下に詰め、はみ出したら左の段へ
-      rest = default_rest = self.height - @margin_top - @margin_bottom
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_height ? ctl.min_height : ctl.height)
+        self.parent.popup(self) if @popup && WS.captured?(@popup)
         
-        rest -= w
-        if rest <= 0
-          if lines[0].empty?
-            lines[0] << ctl
-            lines.unshift []
-          else
-            lines.unshift [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[0] << ctl
-        end
+        super
       end
       
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:bottom, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
-      end
-      @min_width = lines.map(&:min_width).inject(&:+)
-      @min_height = lines.map(&:min_height).max
-      
-      @data = lines
-      
-      hbox
-    end
-    
-    def vcenter_ex #上下に並べ、中央に固める。はみ出したら左の段へ
-      rest = default_rest = self.height - @margin_top - @margin_bottom
-      lines = [[]]
-      
-      @data.each do |ctl|
-        w = (ctl.resizable_height ? ctl.min_height : ctl.height)
+      def on_mouse_out
+        @mouse_on = false
         
-        rest -= w
-        if rest <= 0
-          if lines[0].empty?
-            lines[0] << ctl
-            lines.unshift []
-          else
-            lines.unshift [ctl]
-          end
-          
-          rest = default_rest - w
-        else
-          lines[0] << ctl
-        end
+        super
       end
       
-      lines.delete([])
-      
-      lines.map! do |ary|
-        WSLayout.new(:vcenter, @obj, self) do
-          ary.each do |ctl|
-            add ctl
-          end
-        end
+      def render
+        self.image = @image[@mouse_on]
+        
+        super
       end
-      @min_width = lines.map(&:min_width).inject(&:+)
-      @min_height = lines.map(&:min_height).max
-      
-      @data = lines
-      
-      hbox
     end
     
-    def auto_layout
-      @data = @default_data
+    def initialize(menuitems)
+      super(nil, nil, nil, 16) #オートレイアウトで設定する。
+      @font = Font.new(12)
+      @menuitems = ary = menuitems.map{|ary|
+        mbi = WSMenuBarItem.new(ary[0], ary[1], @font)
+        self.add_control(mbi)
+        mbi
+      }
+      self.image.bgcolor = COLOR[:base]
+      @popup = nil
       
-      self.__send__(@type) if @@types.include?(@type)
+      layout(:left_ex) do
+        ary.each do |item|
+          add item
+        end
+      end
+    end
+    
+    def popup(item)
+      WS.desktop.remove_control(@popup) if @popup
+      tmpx, tmpy = item.get_global_vertex
+      @popup = WSPopupMenu.new(tmpx, @font.size + 4 + tmpy, item.item)
+      WS.desktop.add_control(@popup)
+      @popup.object = self
+      WS.capture(@popup)
+      
+      @menuitems.each do |ctl|
+        ctl.popup = @popup
+      end
+    end
+    
+    def resize(width, height)
+      super
+      self.height = @min_height
+    end
+  end
+end
