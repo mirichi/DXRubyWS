0e924ff2cc13a29c3a9e69a8f97dfcda8272ee51
RPGP1 <great.super.science@gmail.com>
2014-12-15 09:53:14 +0900

    メッセージボックスと確認フォームでダイアログボックスを使用するように更新
    その過程でWindowTitleのフォントを共有するようにしました。

--- lib/StandardGUI/confirmbox.rb
+++ lib/StandardGUI/confirmbox.rb
@@ -1,61 +1,35 @@
 # coding: utf-8
 require_relative './button'
+require_relative './dialogbase'
 require_relative './label'
 require_relative './common'
 
 module WS
-  # システムモーダルなメッセージボックス
-  class WSConfirmBox < WSContainer
-    # ウィンドウのタイトルバー用クラス
-    class WSConfirmBoxTitle < WSContainer
-      def initialize(title="")
-        @font = @@default_font
-        super(nil, nil, nil, @font.size)
-        self.image.bgcolor = [0, 0, 160]
-        
-        # ウィンドウタイトル
-        label = WSLabel.new(nil, nil, nil, nil, title)
-        label.fore_color = C_WHITE
-        label.font = Font.new(@font.size - 2, @font.fontname, :weight=>true)
-        add_control(label)
-        
-        # オートレイアウト
-        layout(:hbox) do
-          self.margin_top = self.margin_right = 1
-          self.margin_left = 2
-          add label
-        end
-      end
-    end
+  # システムモーダルな確認ボックス
+  class WSConfirmBox < WSDialogBase
     
     attr_reader :border_width, :window_focus, :message, :result # ウィンドウ上のフォーカスを持つコントロール
-    include WindowFocus
     
-    def initialize(caption = "", message, cancelable: true)
+    def initialize(caption = "", message, **style)
       @font = @@default_font
-      @border_width = 3
       
-      message_width = @font.get_width(message) + @border_width * 2 + 20 #20はmessageの左右に開けるスペース
+      message_width = @font.get_width(message) + default_border_width * 2 + 20 #20はmessageの左右に開けるスペース
       message_height = @font.size
-      btn_width = @font.get_width("YES") + @font.get_width("NO") + @border_width * 2 + 50
+      btn_width = @font.get_width("YES") + @font.get_width("NO") + default_border_width * 2 + 50
       btn_height = @font.size + 24
-      caption_width = @font.get_width(caption) + @border_width * 2 + 4
-      caption_height = @font.size
+      caption_width = WSWindow::WSWindowTitle.title_font.get_width(caption) + default_border_width * 2 + window_title_height + 1
+      caption_height = window_title_height
       
       sx = [message_width, btn_width, caption_width].max
-      sy = message_height + btn_height + caption_height + 30 + @border_width
+      sy = message_height + btn_height + caption_height + 30 + default_border_width
       tx = (WS.desktop.width - sx) / 2
       ty = (WS.desktop.height - sy) / 2
       
-      super(tx, ty, sx, sy)
-      self.image.bgcolor = COLOR[:base]
+      super(tx, ty, sx, sy, caption, style)
       @message = message
       @result = nil
       
-      window_title = WSConfirmBoxTitle.new(caption)
-      add_control(window_title, :window_title)
-      
-      message_label = WSLabel.new(nil, nil, nil, nil, @message)
+      message_label = WSLabel.new(nil, nil, @font.get_width(@message), nil, @message)
       add_control(message_label, :message_label)
       
       btn_yes = WSButton.new(nil,nil,nil,nil,"YES")
@@ -70,28 +44,21 @@ module WS
       
       # オートレイアウトでコントロールの位置を決める
       # Layout#objで元のコンテナを参照できる
-      layout(:vbox) do
-        self.margin_top = self.margin_left = self.margin_right = self.margin_bottom = self.obj.border_width
-        add window_title
-        layout(:vbox) do
-          self.margin_top = self.margin_bottom = self.space = 10
-          self.margin_left = self.margin_right = 10
-          add message_label
-          layout(:hbox) do
-            self.margin_left = self.margin_right = 5
-            self.space = 20
-            add btn_yes
-            add btn_no
-          end
+      client.layout(:vbox) do
+        self.margin_top = self.margin_bottom = self.space = 10
+        self.margin_left = self.margin_right = 10
+        add message_label
+        layout(:hbox) do
+          self.margin_left = self.margin_right = 5
+          self.space = 20
+          add btn_yes
+          add btn_no
         end
       end
       
-      # Escで閉じる
-      add_key_handler(K_ESCAPE){self.close} if cancelable
-      
       # マウスキャプチャする
       WS.capture(self, true)
-
+      
       # yesにフォーカスをあてる
       btn_yes.activate
     end
